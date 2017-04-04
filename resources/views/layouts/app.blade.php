4d7f9fb913d6d7edb0f11d6e28c5f649e348b864
lishuoz <zhanglishuo13@gmail.com>
2017-04-04 15:18:51 -0400

    Add Google Analytics and Hotjar code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,27 +13,50 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.css">   
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
             ]) !!};
         </script>
-    </head>
-    <body>
-        <div id="app">
-            @section('navbar')
-            <lz-navbar item="projects"></lz-navbar>
-            @show
+        <!-- Hotjar Tracking Code for www.lishuoz.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:465583,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+        <!-- Google Analytics Code for www.lishuoz.com -->
 
-            @yield('content')
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-            <lz-footer></lz-footer>
-        </div>
+          ga('create', 'UA-74984891-3', 'auto');
+          ga('send', 'pageview');
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.js"></script>
-    </body>
-    </html>
+      </script>
+  </head>
+  <body>
+    <div id="app">
+        @section('navbar')
+        <lz-navbar item="projects"></lz-navbar>
+        @show
+
+        @yield('content')
+
+        <lz-footer></lz-footer>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lity/2.2.2/lity.js"></script>
+</body>
+</html>
